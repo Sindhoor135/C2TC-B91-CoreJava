8424708b2b0331becca3ccb4a7b666a4a091e6f7
Sindhoor Hegde <augusthya007@gmail.com>
2024-06-20 22:39:36 +0530

    First commit

---
+++ Batch91/src/com/tnsif/dayone/FirstProgram.java
@@ -0,0 +1,13 @@
+package com.tnsif.dayone;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World..");
+		System.out.print("Abc");
+		System.out.println("good morning");
+
+	}
+
+}
---
+++ Batch91/src/com/tnsif/dayone/UserName.java
@@ -0,0 +1,15 @@
+package com.tnsif.dayone;
+import java.util.*;
+
+public class UserName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter your Name:");
+	    String a=input.nextLine();
+	    System.out.println("Your name="+a);
+		
+	}
+
+}
